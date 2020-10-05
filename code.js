6f186805d12ca28f4045a39c06cab926c77ff5d4
salman khan lodhi <71936828+salmanlodhi0332@users.noreply.github.com>
2020-10-05 19:55:44 +0500

    Add files via upload

---
+++ code.js
@@ -0,0 +1,15 @@
+let arry=[2,3,4];
+
+let loop,i;
+
+for(loop=0;loop<arry.length;loop++)
+{
+ 
+    console.log("\t\tTABLE OF ",arry[loop])
+
+    for(i=1;i<11;i++)
+{
+  console.log(arry[loop]," * ",i," = ",arry[loop]*i)
+}
+
+}
